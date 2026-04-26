278ddfdc2d9316b984b53dc631eb6d87032bb095
castropatrick <patrickcastro73@gmail.com>
2026-04-26 03:00:25 -0300

    Configurando o handler de notificações

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,21 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { View, LogBox } from 'react-native';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n';
+import * as Notifications from 'expo-notifications';
+
+
+LogBox.ignoreLogs(['expo-notifications: Android Push notifications']);
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function Layout() {
   return (
